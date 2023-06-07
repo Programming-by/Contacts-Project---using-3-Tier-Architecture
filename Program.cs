2364e7a90ad251c3e3aa39c2ef1ac6886cd949c5
AnasNaamneh <naamnih.anas@gmail.com>
2023-06-06 21:24:24 -0700

    Add New Contact

--- Program.cs
+++ Program.cs
@@ -33,9 +33,30 @@ namespace ContactsConsoleApp___Presentation_Layer
 
         }
 
+        public static void testAddNewContact()
+        {
+            clsContacts Contact1 = new clsContacts();
+
+            Contact1.FirstName = "Anas";
+            Contact1.LastName = "Naamneh";
+            Contact1.Email = "naamneh.anas@gmail.com";
+            Contact1.Phone = "07888888";
+            Contact1.Address = "Amman";
+            Contact1.DateOfBirth = new DateTime(1977,11,6,10,30,0);
+            Contact1.CountryID = 1;
+            Contact1.ImagePath = "";
+
+            if (Contact1.Save())
+            {
+                Console.WriteLine("Contact Added Successfully with id=" + Contact1.ID);
+            }
+
+
+        }
         static void Main(string[] args)
         {
-            testFindContact(1);
+            //  testFindContact(2);
+            testAddNewContact();
 
             Console.ReadKey();
         }
